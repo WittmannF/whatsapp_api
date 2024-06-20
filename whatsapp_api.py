fc628f55d749a215d2bf6393c7d6b80c80963dcd
Fernando Wittmann <fernando.wittmann@gmail.com>
2024-06-20 20:53:17 -0300

    Hotfix: Requirements and Xpaths - Add Selenium 3 to requirements for compatibility - Update xpaths to the latest version

--- whatsapp_api.py
+++ whatsapp_api.py
@@ -12,9 +12,9 @@ WP_LINK = 'https://web.whatsapp.com'
 CONTACTS = '//*[@id="main"]/header/div[2]/div[2]/span'
 MESSAGE_BOX = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
 SEND = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'
-NEW_CHAT = '//*[@id="app"]/div/div/div[3]/header/div[2]/div/span/div[3]/div'
-FIRST_CONTACT = '//*[@id="app"]/div/div/div[2]/div[1]/span/div/span/div/div[2]/div/div/div/div[2]/div'
-SEARCH_CONTACT = '//*[@id="app"]/div/div/div[2]/div[1]/span/div/span/div/div[1]/div/div/div[2]/div/div[2]'
+NEW_CHAT = '//*[@id="app"]/div/div[2]/div[3]/header/header/div/span/div/span/div[1]/div/span'
+FIRST_CONTACT = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[2]/div/div/div/div[2]/div/div/div[2]'
+SEARCH_CONTACT = '//*[@id="app"]/div/div[2]/div[2]/div[1]/span/div/span/div/div[1]/div[2]/div[2]/div/div[1]'
 
 class WhatsApp:
     def __init__(self):
